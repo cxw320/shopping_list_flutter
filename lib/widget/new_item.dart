8a7a95ee3a0613b5d2143bed77ad456fa07cd95a
Caroline Wang <cxw320@case.edu>
2023-09-16 13:44:17 -0400

    add firebase

--- lib/widget/new_item.dart
+++ lib/widget/new_item.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import '../data/categories.dart';
 import '../models/category.dart';
@@ -21,6 +24,21 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
+      final url = Uri.https('flutter-prep-9a451-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode(
+          {
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
+        ),
+      );
       Navigator.of(context).pop(
         GroceryItem(
           id: DateTime.now.toString(),
